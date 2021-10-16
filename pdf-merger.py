34c99799371273c02ca26bb151236e45021160c5
Ling-Jyh Chen <cclljj@gmail.com>
2021-10-16 14:55:53 +0800

    add two black bars on the left and right sides for quick indexing

--- pdf-merger.py
+++ pdf-merger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from argparse import ArgumentParser
 import re
-from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 import io
+import os
+from argparse import ArgumentParser
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
@@ -27,37 +28,78 @@ def merge_QA(output_file, q_folder, a_folder):
 	output.addBlankPage(width=595, height=842)
 	output.write(open("/tmp/blank.pdf", 'wb'))
 
+	index = 0
 	for C in QA:
+		index = index + 1
+		index10 = ((index-1) // 10) % 10
+
+		str_FILE_Q = q_folder+"/"+C+"-Q.pdf"
+		str_FILE_A = a_folder+"/"+QA[C]+"-A.pdf"
+		str_TMP_Q = "/tmp/"+C+"-Q.pdf"
+		str_TMP_A = "/tmp/"+C+"-A.pdf"
+
 		if p%2==0:
 			p = p + 1
 			merger.append("/tmp/blank.pdf")
 
 		packet = io.BytesIO()
 		can = canvas.Canvas(packet)
-		#can.drawString(10, 820, "Final: "+C+"; Original: "+QA[C])
-		can.drawString(10, 820, "Case #: "+C)
+		can.setFont('Helvetica', 20)
+		can.drawString(10, 820, "#" + str(index) + ": " + C)
+		#add two black rectangles on the two sides for quick indexing
+		can.rect(0,800-40*index10-40,5,40,fill=1,stroke=1)
+		can.rect(590,800-40*index10-40,5,40,fill=1,stroke=1)
 		can.save()
 		packet.seek(0)
 
-		new_pdf = PdfFileReader(packet)
-		existing_pdf = PdfFileReader(open(q_folder+"/"+C+"-Q.pdf", "rb"))
-		output = PdfFileWriter()
-		for i in range (0,existing_pdf.getNumPages(),1):
-			page = existing_pdf.getPage(i)
-			# add the "watermark" (which is the new pdf) on the existing page
-			page.mergePage(new_pdf.getPage(0))
-			output.addPage(existing_pdf.getPage(i))
-		outputStream = open("/tmp/"+C+"-Q.pdf", "wb")
+		if os.path.isfile(str_FILE_Q):
+			new_pdf = PdfFileReader(packet)
+			existing_pdf = PdfFileReader(open(str_FILE_Q, "rb"))
+			output = PdfFileWriter()
+			for i in range (0,existing_pdf.getNumPages(),1):
+				page = existing_pdf.getPage(i)
+				# add the "watermark" (which is the new pdf) on the existing page
+				page.mergePage(new_pdf.getPage(0))
+				output.addPage(existing_pdf.getPage(i))
+		else:
+			print("Error - file missing: ", str_FILE_Q)
+
+		outputStream = open(str_TMP_Q, "wb")
 		output.write(outputStream)
 		outputStream.close()
 
-		inputStream = open("/tmp/"+C+"-Q.pdf",'rb')
-		p = p + PdfFileReader(inputStream).getNumPages()
-		inputStream.close()
-		merger.append("/tmp/"+C+"-Q.pdf")
+		if os.path.isfile(str_TMP_Q):
+			inputStream = open(str_TMP_Q,'rb')
+			p = p + PdfFileReader(inputStream).getNumPages()
+			inputStream.close()
+			merger.append(str_TMP_Q)
+		else:
+			print("Error - file missing: ", str_TMP_Q)
 
-		p = p + PdfFileReader(open(a_folder+"/"+QA[C]+"-A.pdf",'rb')).getNumPages()
-		merger.append(a_folder+"/"+QA[C]+"-A.pdf")
+		if os.path.isfile(str_FILE_A):
+			new_pdf = PdfFileReader(packet)
+			existing_pdf = PdfFileReader(open(str_FILE_A, "rb"))
+			output = PdfFileWriter()
+			for i in range (0,existing_pdf.getNumPages(),1):
+				page = existing_pdf.getPage(i)
+				# add the "watermark" (which is the new pdf) on the existing page
+				page.mergePage(new_pdf.getPage(0))
+				output.addPage(existing_pdf.getPage(i))
+		else:
+			print("Error - file missing: ", str_FILE_A)
+
+		outputStream = open(str_TMP_A, "wb")
+		output.write(outputStream)
+		outputStream.close()
+
+		if os.path.isfile(str_TMP_A):
+			inputStream = open(str_TMP_A,'rb')
+			p = p + PdfFileReader(inputStream).getNumPages()
+			inputStream.close()
+			merger.append(str_TMP_A)
+		else:
+			print("Error - file missing: ", str_TMP_A)
+
 
 	merger.write(output_file)
 	merger.close()
